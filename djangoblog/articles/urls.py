5eca35a5af812fba4f7364cd438b23114ac026be
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-03-20 20:07:36 +0530

    create article and redirect

--- djangoblog/articles/urls.py
+++ djangoblog/articles/urls.py
@@ -7,5 +7,6 @@ app_name = 'articles'
 
 urlpatterns = [
     url('^$', views.article_list, name="list"),
+    url('^create/$', views.article_create, name="create"),
     url('^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
