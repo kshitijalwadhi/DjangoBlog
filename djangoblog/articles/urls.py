e8cb3c0eb9a0ab0038c7d15ecf417c77091f40c7
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-03-20 10:58:00 +0530

    named urls

--- djangoblog/articles/urls.py
+++ djangoblog/articles/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    url('^$', views.article_list),
+    url('^$', views.article_list, name="list"),
+    url('^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
